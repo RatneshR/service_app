d15c0a254cffd9a829ec02b3d4f561c0035bd5c9
RatneshR <ratnesh.rai26@gmailcom>
2025-02-18 22:34:56 +0530

    First Commit

---
+++ src/test/java/com/dev/serviceApp/ServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.serviceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
