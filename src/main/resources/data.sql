beda1efd6c8f6037ca7ee10268bf1781c0525795
RatneshR <ratnesh.rai26@gmailcom>
2025-06-29 18:16:28 +0530

    Changes for Postgres database table creation

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO widgets(id, name, purpose) VALUES
+(1, 'Widget A', 'To demonstrate widget functionality'),
+(2, 'Widget B', 'To showcase widget capabilities'),
+(3, 'Widget C', 'To illustrate widget usage'),
+(4, 'Widget D', 'To exemplify widget features'),
+(5, 'Widget E', 'To highlight widget advantages');
