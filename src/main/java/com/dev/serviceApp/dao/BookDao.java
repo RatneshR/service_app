44571673751b6610ea2e403f317bd367c7063b2f
RatneshR <ratnesh.rai26@gmailcom>
2025-07-01 09:10:45 +0530

    Changes for Mocikto and unit test creation

--- src/main/java/com/dev/serviceApp/dao/BookDao.java
+++ src/main/java/com/dev/serviceApp/dao/BookDao.java
@@ -1,4 +1,7 @@
 package com.dev.serviceApp.dao;
 
+import com.dev.serviceApp.domain.Books;
+
 public interface BookDao {
+    void create(Books book);
 }
--- src/main/java/com/dev/serviceApp/domain/Books.java
+++ src/main/java/com/dev/serviceApp/domain/Books.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class Books {
-    private Long isbn;
+    private String isbn;
     private String name;
     private Long author_id;
 }
