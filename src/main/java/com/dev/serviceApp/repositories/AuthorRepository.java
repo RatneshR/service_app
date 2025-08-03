305811bf42c6243770c16f1646bf48b24f2699c1
Ratnesh Rai <ratnesh.rai26@gmail.com>
2025-08-03 14:31:44 +0530

    Repository changes

--- src/main/java/com/dev/serviceApp/repositories/AuthorRepository.java
+++ src/main/java/com/dev/serviceApp/repositories/AuthorRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AuthorRepository  extends CrudRepository<Authors, Long> {
 
+    Iterable<Authors> ageLessThan(Integer age);
+
 }
--- src/main/java/com/dev/serviceApp/repositories/BookRepository.java
+++ src/main/java/com/dev/serviceApp/repositories/BookRepository.java
@@ -1,4 +1,11 @@
 package com.dev.serviceApp.repositories;
 
-public interface BookRepository {
+import com.dev.serviceApp.domain.Books;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Books, String> {
+
+    // Additional query methods can be defined here if needed
+    // For example, to find books by author name:
+    // List<Books> findByAuthorName(String authorName);
 }
