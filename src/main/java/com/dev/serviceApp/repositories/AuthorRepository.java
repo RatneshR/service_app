81cb9dc067f7ecf26d196898d362a8623d4c236a
Ratnesh Rai <ratnesh.rai26@gmail.com>
2025-08-03 18:37:49 +0530

    Test and Stable build changes

--- src/main/java/com/dev/serviceApp/repositories/AuthorRepository.java
+++ src/main/java/com/dev/serviceApp/repositories/AuthorRepository.java
@@ -1,12 +1,16 @@
 package com.dev.serviceApp.repositories;
 
-import com.dev.serviceApp.domain.Authors;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorRepository  extends CrudRepository<Authors, Long> {
-
-    Iterable<Authors> ageLessThan(Integer age);
-
-}
+//import com.dev.serviceApp.domain.Authors;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface AuthorRepository  extends CrudRepository<Authors, Long> {
+//
+//    Iterable<Authors> ageLessThan(Integer age);
+//
+//    @Query("SELECT a FROM Authors a WHERE a.age > ?1")
+//    Iterable<Authors> ageGreaterThan(Integer age);
+//
+//}
